fb3ef4ca464c1842549510f46b65d3e64c400651
Jared Grippe <jared@deadlyicon.com>
2020-09-29 12:08:37 -0700

    moved ./index.js to ./jwt/index.js

--- jwt/index.js
+++ jwt/index.js
@@ -9,39 +9,39 @@ class JlincJwtError extends Error {
 };
 
 module.exports =  {
-  version: require('./package.json').version,
+  version: require('../package.json').version,
   JlincJwtError,
 
   // Sign with HMAC
   // signHmac(PayloadObject, SecretString) -> JWT
-  signHmac: require('./jwt/signHmac'),
+  signHmac: require('./signHmac'),
 
   // Sign with EdDSA subtype ed25519
   // signEdDsa(PayloadObject, PublicKey, SecretKey[, DIDkeyUrl]) -> JWT
-  signEdDsa: require('./jwt/signEdDsa'),
+  signEdDsa: require('./signEdDsa'),
 
   // Sign with HMAC via compatibility function
   // sign(PayloadObject, SecretString) -> JWT
-  sign: require('./jwt/signCompat'),
+  sign: require('./signCompat'),
 
   // Verify HMAC
   // verifyHmac(JWT, SecretString) -> decoded JWT
-  verifyHmac: require('./jwt/verifyHmac'),
+  verifyHmac: require('./verifyHmac'),
 
   // Verify EdDSA subtype ed25519
   // verifyEdDsa(JWT[, PublicKey]) -> decoded JWT
   // N.B. public key must be in header as jwk:k or as second arg
-  verifyEdDsa: require('./jwt/verifyEdDsa'),
+  verifyEdDsa: require('./verifyEdDsa'),
 
   // Verify assuming HMAC
   // verify(JWT, SecretString) -> decoded JWT
-  verify: require('./jwt/verifyCompat'),
+  verify: require('./verifyCompat'),
 
   // Decode JWT
   // read(JWT) -> decoded JWT
-  read: require('./jwt/read'),
+  read: require('./read'),
 
   // Decode JWT with compatibility default options -> {complete:false}
   // decode(JWT) -> decoded JWT payload
-  decode: require('./jwt/decode'),
+  decode: require('./decode'),
 };
